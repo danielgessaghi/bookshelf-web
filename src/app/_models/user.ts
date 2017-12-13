1070f24d4267f543030dabd77d569fb9b94c9ef4
luk95 <beppe1995@libero.it>
2017-12-13 11:04:53 +0100

    form di login e register angular cli

---
+++ src/app/_models/user.ts
@@ -0,0 +1,17 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    //email: string;
+    firstName: string;
+    lastName: string;
+    /*
+    phone: number;
+    groupid: number=1;
+    cap: number;
+    city: string;
+    country: string;
+    street: string;
+    creationDate: DateTimeFormat;
+    */
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import '../assets/app.css';
+
 @Component({
+  moduleId: module.id.toString(),
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: "app";
 }
