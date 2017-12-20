4916960c81dc6bdf884c9c8da394135c28d47631
danielgessaghi <daniel.gessaghi19971@gmail.com>
2017-12-20 17:40:07 +0100

    tutto fatto forse

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -33,11 +33,13 @@ export class BooksComponent implements OnInit {
   p: number = 1;
   lock: boolean = false;
   constructor(private http: HttpClient, private pagerService: PagerService) { }
-
+  public category_button:string = '/bookshelf-api/public/start.php/api/category/list';
+ 
   public ip_address: string = '/bookshelf-api/public/start.php/api/books/list/';
   
   // array of all items to be paged
   private allItems: any[];
+  private Categories:any[];
 
   // pager object
   pager: any = {}
@@ -49,22 +51,50 @@ export class BooksComponent implements OnInit {
     this.pagedItems = [];
     // get dummy data
     this.loadNextPage();
+
+    this.Categories = [];
+    this.loadCategories();
   }
+  loadCategories(){
+    this.http.get<Array<any>>(this.category_button)
+        .subscribe(data => {
+          this.Categories = data;
+        });
+  }
+  private SortBy: string;
+  sort(model:string){
+    this.SortBy = model;
+    return this.SortBy;
+  }
+  public api_sort ='/bookshelf-api/public/start.php/api/category/sorted/';
   loadNextPage() {
     if (!this.lock) {
       this.lock = true;
       this.http.get<Array<any>>(this.ip_address + this.p)
         .subscribe(data => {
           // set items to json response
-          this.pagedItems = this.pagedItems.concat(data);
-          this.p = this.p + 1;
-          this.lock = false;
+          if (this.SortBy != null) {
+
+            let path = this.api_sort.concat(this.SortBy)
+            this.http.post(path,this.model)
+            .subscribe(ret=>{
+              this.pagedItems = this.pagedItems.concat(ret);
+              this.p = this.p + 1;
+              this.lock = false;
+            });
+          }
+          else{
+            this.pagedItems = this.pagedItems.concat(data);
+            this.p = this.p + 1;
+            this.lock = false;
+          }
         });
     }
   }
   onScrollDown() {
     this.loadNextPage();
   }
+  
 
   public add_item_api: string = '/bookshelf-api/public/start.php/api/cart/add/';
   addItem(isbn:string){
