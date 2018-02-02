1c8053d2610e876255a0497a1463eecdfb48f73f
danielgessaghi <daniel.gessaghi19971@gmail.com>
2018-02-02 19:44:15 +0100

    some changes in cartList see the api

--- src/app/_models/cartList.ts
+++ src/app/_models/cartList.ts
@@ -1,11 +1,17 @@
 import { Book } from "./book";
 import { User } from "./index";
 import { AuthenticationService } from "../_services/authentication.service"
+import { getLocaleDateTimeFormat } from "@angular/common/src/i18n/locale_data_api";
+import { Data } from "@angular/router/src/config";
 export class CartItem {
     constructor() {
         this.ID_ORDER = 0;
         this.QUANTITY = 0;
+        this.TOT_PRICE = 0;
+        this.ORDER_DATE = new Date().toISOString; 
     }
+    ORDER_DATE: Data;
+    TOT_PRICE:number;
     ID_ORDER: number;
     BOOK: Book;
     QUANTITY: number;
